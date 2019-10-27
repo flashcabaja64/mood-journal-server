2019e0ea276a2b4669a203f4ee6a397b8c39c861
Hubert Yang <offspring7890@yahoo.com>
2019-10-27 12:31:49 -0700

    changes

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,3 +1,5 @@
+process.env.SSL = true;
+
 require('dotenv').config()
 
 module.exports = {
@@ -5,5 +7,6 @@ module.exports = {
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL
+    : process.env.DATABASE_URL,
+  "ssl": !!process.env.SSL
 }
